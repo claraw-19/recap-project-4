6aebff0e496da49ce9931fc6d52c558a9e5aaf60
Clara Weyer <clara.weyer@schullv.de>
2024-12-02 11:44:30 +0100

    develop ColorInput component

---
+++ src/Components/ColorInput/ColorInput.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export function ColorInput({ id, value }) {
+  const [inputValue, setInputValue] = useState(value);
+  function handleColorChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        id={id}
+        name={id}
+        value={value}
+        onChange={handleColorChange}
+      />
+      <input type="color" value={inputValue} onChange={handleColorChange} />
+    </div>
+  );
+}
