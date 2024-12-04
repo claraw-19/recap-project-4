40bf3416c58f95c0cebc40bbe9c7f67a115e4bf7
Clara Weyer <clara.weyer@schullv.de>
2024-12-04 09:17:46 +0100

    add dropdown and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
-// import { useState } from "react";
+import { useState } from "react";
 import { nanoid } from "nanoid";
 import { ColorForm } from "./Components/ColorForm/ColorForm";
 import useLocalStorageState from "use-local-storage-state";
+import { initialThemes } from "./lib/themes";
 
 function App() {
   const [colors, setColors] = useLocalStorageState("themecolors", {
     defaultValue: initialColors,
   });
   // console.log(initialColors);
+  const [selectedTheme, setSelectedTheme] = useState(initialThemes[0]);
 
   const addColor = (newColor) => {
     const newColorWithId = { id: nanoid(), ...newColor };
@@ -29,9 +31,27 @@ function App() {
     setColors(changedColors);
   };
 
+  const handleChange = (event) => {
+    const selectedId = event.target.value;
+    const theme = initialThemes.find((theme) => theme.id === selectedId);
+    if (theme) {
+      setSelectedTheme(theme);
+    }
+  };
+
   return (
     <>
       <h1>Theme Creator</h1>
+      <select onChange={handleChange}>
+        {initialThemes.map((theme) => (
+          <option key={theme.id} value={theme.id}>
+            {theme.name}
+          </option>
+        ))}
+      </select>
+      <button className="button--green">Add</button>
+      <button>Edit</button>
+      <button className="button--red">Remove</button>
       <ColorForm onSubmitColor={addColor} buttonText={"Add color"} />
       {colors.length === 0 ? (
         <p>There are no colors, add some!</p>
---
+++ src/lib/themes.js
@@ -0,0 +1,80 @@
+export const initialThemes = [
+  {
+    id: "t1",
+    name: "default theme",
+    colors: [
+      {
+        id: "c1",
+        role: "primary main",
+        hex: "#ff4a11",
+        contrastText: "#FFFFFF",
+      },
+      {
+        id: "c2",
+        role: "primary dark",
+        hex: "#c73e0b",
+        contrastText: "#FFFFFF",
+      },
+      {
+        id: "c3",
+        role: "primary light",
+        hex: "#ff7a3e",
+        contrastText: "#000000",
+      },
+      {
+        id: "c4",
+        role: "secondary main",
+        hex: "#5C6BC0",
+        contrastText: "#FFFFFF",
+      },
+      {
+        id: "c5",
+        role: "secondary dark",
+        hex: "#3949AB",
+        contrastText: "#FFFFFF",
+      },
+      {
+        id: "c6",
+        role: "secondary light",
+        hex: "#9FA8DA",
+        contrastText: "#000000",
+      },
+      {
+        id: "c7",
+        role: "background main",
+        hex: "#252629",
+        contrastText: "#FFFFFF",
+      },
+      {
+        id: "c8",
+        role: "background dark",
+        hex: "#1b1d1f",
+        contrastText: "#FFFFFF",
+      },
+      {
+        id: "c9",
+        role: "background light",
+        hex: "#43464b",
+        contrastText: "#FFFFFF",
+      },
+    ],
+  },
+  {
+    id: "t2",
+    name: "cooperate design",
+    colors: [
+      {
+        id: "c10",
+        role: "math",
+        hex: "#44B350",
+        contrastText: "#e0e0e0",
+      },
+      {
+        id: "c11",
+        role: "biology",
+        hex: "#a0321e",
+        contrastText: "#e0e0e0",
+      },
+    ],
+  },
+];
